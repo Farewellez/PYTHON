34b996bb8cebb0268507f3fefc79afd658543003
Farewellez <ahmadzafarell55@gmail.com>
2025-05-06 15:51:45 +0700

    Selesai untuk latihan palindromrekursif

--- Latihan Function/mian.py
+++ Latihan Function/mian.py
@@ -1,2 +1,24 @@
-numbers = [1]
-print([[2] + [3]])
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+container = []
+for row in range(0,9,3):
+    for column in range(0,9,3):
+
+        for i in range(3):
+            for j in range(3):
+                number = sudoku_board[row + i][column + j]
+                if number != 0:
+                    container.append(number)
+if len(set(container)) != len(container):
+    print(False)
+else:
+    print(True)
---
+++ Latihan Palindrom Rekursif/main.py
@@ -0,0 +1,3 @@
+# Jadi file ini bisa buat coba-coba dulu 
+# karena biasanya jika file ipynb mirip compiler jadi langsung menjalankan semuanya
+# Jadi agak susah buat debugging
